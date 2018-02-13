7d8e8d6eee141f2cd7d086bd13a1741b6a96855c
cmep125 <35964735+cmep125@users.noreply.github.com>
2018-02-13 11:13:10 -0500

    Add files via upload

---
+++ ENGR122_Final_Project.m
@@ -0,0 +1,410 @@
+template(1).name   = '1Y01'
+template(1).RValue = 209
+template(1).GValue = 181
+template(1).BValue = 167
+
+template(2).name   = '2Y01'
+template(2).RValue = 212
+template(2).GValue = 181
+template(2).BValue = 161
+
+template(3).name   = '3Y01'
+template(3).RValue = 212
+template(3).GValue = 181
+template(3).BValue = 160
+
+template(4).name   = '4Y01'
+template(4).RValue = 212
+template(4).GValue = 182
+template(4).BValue = 156
+
+template(5).name   = '5Y01'
+template(5).RValue = 212
+template(5).GValue = 182
+template(5).BValue = 158
+
+template(6).name   = '1R02'
+template(6).RValue = 209
+template(6).GValue = 176
+template(6).BValue = 159
+
+template(7).name   = '1Y02'
+template(7).RValue = 205
+template(7).GValue = 176
+template(7).BValue = 160
+
+template(8).name   = '2Y02'
+template(8).RValue = 208
+template(8).GValue = 175
+template(8).BValue = 156
+
+template(9).name   = '3Y02'
+template(9).RValue = 207
+template(9).GValue = 176
+template(9).BValue = 155
+
+template(10).name   = '4Y02'
+template(10).RValue = 207
+template(10).GValue = 177
+template(10).BValue = 153
+
+template(11).name   = '5Y02'
+template(11).RValue = 204
+template(11).GValue = 176
+template(11).BValue = 152
+
+template(12).name   = '2R03'
+template(12).RValue = 208
+template(12).GValue = 171
+template(12).BValue = 155
+
+template(13).name   = '1R03'
+template(13).RValue = 204
+template(13).GValue = 171
+template(13).BValue = 156
+
+template(14).name   = '1Y03'
+template(14).RValue = 203
+template(14).GValue = 170
+template(14).BValue = 153
+
+template(15).name   = '2Y03'
+template(15).RValue = 205
+template(15).GValue = 173
+template(15).BValue = 152
+
+template(16).name   = '3Y03'
+template(16).RValue = 204
+template(16).GValue = 173
+template(16).BValue = 152
+
+template(17).name   = '4Y03'
+template(17).RValue = 203
+template(17).GValue = 172
+template(17).BValue = 151
+
+template(18).name   = '5Y03'
+template(18).RValue = 201
+template(18).GValue = 173
+template(18).BValue = 149
+
+template(19).name   = '5304'
+template(19).RValue = 197
+template(19).GValue = 158
+template(19).BValue = 151
+
+template(20).name   = '4R04'
+template(20).RValue = 197
+template(20).GValue = 160
+template(20).BValue = 151
+
+template(21).name   = '3R04'
+template(21).RValue = 195
+template(21).GValue = 161
+template(21).BValue = 149
+
+template(22).name   = '2R04'
+template(22).RValue = 197
+template(22).GValue = 161
+template(22).BValue = 147
+
+template(23).name   = '1R04'
+template(23).RValue = 194
+template(23).GValue = 162
+template(23).BValue = 149
+
+template(24).name   = '1Y04'
+template(24).RValue = 194
+template(24).GValue = 162
+template(24).BValue = 147
+
+template(25).name   = '2Y04'
+template(25).RValue = 194
+template(25).GValue = 161
+template(25).BValue = 144
+
+template(26).name   = '3Y04'
+template(26).RValue = 191
+template(26).GValue = 162
+template(26).BValue = 144
+
+template(27).name   = '4Y04'
+template(27).RValue = 191
+template(27).GValue = 163
+template(27).BValue = 142
+
+template(28).name   = '5Y04'
+template(28).RValue = 190
+template(28).GValue = 163
+template(28).BValue = 142
+
+template(29).name   = '5R05'
+template(29).RValue = 192
+template(29).GValue = 154
+template(29).BValue = 145
+
+template(30).name   = '4R05'
+template(30).RValue = 193
+template(30).GValue = 153
+template(30).BValue = 143
+
+
+template(31).name   = '3R05'
+template(31).RValue = 193
+template(31).GValue = 155
+template(31).BValue = 142
+
+
+template(32).name   = '2R05'
+template(32).RValue = 193
+template(32).GValue = 155
+template(32).BValue = 142
+
+
+template(33).name   = '1R05'
+template(33).RValue = 192
+template(33).GValue = 156
+template(33).BValue = 142
+
+
+template(34).name   = '1Y05'
+template(34).RValue = 191
+template(34).GValue = 156
+template(34).BValue = 137
+
+
+template(35).name   = '2Y05'
+template(35).RValue = 191
+template(35).GValue = 156
+template(35).BValue = 136
+
+
+template(36).name   = '3Y05'
+template(36).RValue = 188
+template(36).GValue = 157
+template(36).BValue = 137
+
+
+template(37).name   = '4Y05'
+template(37).RValue = 186
+template(37).GValue = 158
+template(37).BValue = 136
+
+
+template(38).name   = '5Y05'
+template(38).RValue = 184
+template(38).GValue = 159
+template(38).BValue = 139
+
+
+template(39).name   = '5R06'
+template(39).RValue = 193
+template(39).GValue = 149
+template(39).BValue = 138
+
+
+template(40).name   = '4R06'
+template(40).RValue = 192
+template(40).GValue = 148
+template(40).BValue = 135
+
+template(41).name   = '3R06'
+template(41).RValue = 191
+template(41).GValue = 149
+template(41).BValue = 135
+
+template(42).name   = '2R06'
+template(42).RValue = 191
+template(42).GValue = 150
+template(42).BValue = 132
+
+template(43).name   = '1R06'
+template(43).RValue = 187
+template(43).GValue = 150
+template(43).BValue = 132
+
+template(44).name   = '1Y06'
+template(44).RValue = 186
+template(44).GValue = 149
+template(44).BValue = 130
+
+template(45).name   = '2Y06'
+template(45).RValue = 186
+template(45).GValue = 149
+template(45).BValue = 130
+
+template(46).name   = '3Y06'
+template(46).RValue = 184
+template(46).GValue = 152
+template(46).BValue = 131
+
+template(47).name   = '4Y06'
+template(47).RValue = 183
+template(47).GValue = 153
+template(47).BValue = 127
+
+template(48).name   = '5Y06'
+template(48).RValue = 183
+template(48).GValue = 153
+template(48).BValue = 127
+
+
+template(49).name   = '5R07'
+template(49).RValue = 184
+template(49).GValue = 142
+template(49).BValue = 130
+
+template(50).name   = '4R07'
+template(50).RValue = 184
+template(50).GValue = 140
+template(50).BValue = 129
+
+template(51).name   = '3R07'
+template(51).RValue = 184
+template(51).GValue = 140
+template(51).BValue = 127
+
+template(52).name   = '2R07'
+template(52).RValue = 183
+template(52).GValue = 141
+template(52).BValue = 125
+
+template(53).name   = '1R07'
+template(53).RValue = 182
+template(53).GValue = 144
+template(53).BValue = 125
+
+template(54).name   = '1Y07'
+template(54).RValue = 182
+template(54).GValue = 144
+template(54).BValue = 125
+
+template(55).name   = '2Y07'
+template(55).RValue = 180
+template(55).GValue = 144
+template(55).BValue = 122
+
+template(56).name   = '3Y07'
+template(56).RValue = 180
+template(56).GValue = 144
+template(56).BValue = 118
+
+template(57).name   = '4Y07'
+template(57).RValue = 177
+template(57).GValue = 146
+template(57).BValue = 118
+
+template(58).name   = '5Y07'
+template(58).RValue = 177
+template(58).GValue = 146
+template(58).BValue = 117
+
+template(59).name   = '4R08'
+template(59).RValue = 180
+template(59).GValue = 132
+template(59).BValue = 118
+
+template(60).name   = '3R08'
+template(60).RValue = 180
+template(60).GValue = 133
+template(60).BValue = 117
+
+template(61).name   = '2R08'
+template(61).RValue = 180
+template(61).GValue = 133
+template(61).BValue = 113
+
+template(62).name   = '1R08'
+template(62).RValue = 180
+template(62).GValue = 134
+template(62).BValue = 111
+
+template(63).name   = '1Y08'
+template(63).RValue = 179
+template(63).GValue = 135
+template(63).BValue = 110
+
+template(64).name   = '2Y08'
+template(64).RValue = 177
+template(64).GValue = 136
+template(64).BValue = 108
+
+template(65).name   = '3Y08'
+template(65).RValue = 173
+template(65).GValue = 136
+template(65).BValue = 110
+
+template(66).name   = '4Y08'
+template(66).RValue = 172
+template(66).GValue = 138
+template(66).BValue = 111
+
+template(67).name   = '5Y08'
+template(67).RValue = 170
+template(67).GValue = 139
+template(67).BValue = 110
+
+template(68).name   = '4R09'
+template(68).RValue = 170
+template(68).GValue = 126
+template(68).BValue = 113
+
+template(69).name   = '3R09'
+template(69).RValue = 172
+template(69).GValue = 126
+template(69).BValue = 110
+
+template(70).name   = '2R09'
+template(70).RValue = 172
+template(70).GValue = 127
+template(70).BValue = 108
+
+template(71).name   = '1R09'
+template(71).RValue = 172
+template(71).GValue = 127
+template(71).BValue = 104
+
+template(72).name   = '1Y09'
+template(72).RValue = 168
+template(72).GValue = 126
+template(72).BValue = 102
+
+template(73).name   = '2Y09'
+template(73).RValue = 168
+template(73).GValue = 128
+template(73).BValue = 102
+
+template(74).name   = '3Y09'
+template(74).RValue = 165
+template(74).GValue = 130
+template(74).BValue = 100
+
+template(75).name   = '4Y09'
+template(75).RValue = 164
+template(75).GValue = 129
+template(75).BValue = 101
+
+template(76).name   = '5Y09'
+template(76).RValue = 162
+template(76).GValue = 131
+template(76).BValue = 100
+
+template(77).name   = '3R10'
+template(77).RValue = 164
+template(77).GValue = 116
+template(77).BValue = 96
+
+template(78).name   = '2R10'
+template(78).RValue = 164
+template(78).GValue = 116
+template(78).BValue = 94
+
+template(79).name   = '1R10'
+template(79).RValue = 163
+template(79).GValue = 118
+template(79).BValue = 95
+
+template(80).name   = '1Y10'
+template(80).RValue = 162
+template(80).GValue = 118
+template(80).BValue = 93
